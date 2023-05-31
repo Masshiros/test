d2cbb027a1bf26bdee80970765a1cf8ba92f3421
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-05-31 09:33:27 +0700

    Add middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-
+router.use((req, res, next) => {
+	console.log( 'Time:' , Date.now());
+	next();
+});
 router.get("/home", (req, res) => {
   res.send("Hello World, This is home router");
 });
