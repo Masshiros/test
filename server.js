ec3bdabcb03108b8267677089fc1c3a82f605a29
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-05-31 08:21:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const router = express.Router();
+
+router.get("/home", (req, res) => {
+  res.send("Hello World, This is home router");
 });
-app.listen(3000, () => {
-  console.log("Server start on port 3000");
+
+router.get("/profile", (req, res) => {
+  res.send("Hello World, This is profile router");
 });
+
+router.get("/login", (req, res) => {
+  res.send("Hello World, This is login router");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("Hello World, This is logout router");
+});
+
+app.use("/", router);
+
+app.listen(process.env.port || 3000);
+
+console.log("Web Server is listening at port " + (process.env.port || 3000));
